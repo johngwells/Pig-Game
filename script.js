8ca4667fa54e18e9f08513f6d6ffed01385c485b
John Wells <molekewl2017@gmail.com>
2021-03-08 11:18:06 -0800

    logic | set active player

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const current0El = document.getElementById('current--0')
-const current1El = document.getElementById('current--1')
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -14,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function() {
   // Generate a random dice roll
@@ -28,8 +32,13 @@ btnRoll.addEventListener('click', function() {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore
   } else {
     // Switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
