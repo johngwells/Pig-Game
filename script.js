41cb6fccd4c2d0da93d519562d75cdcea2978b58
John Wells <molekewl2017@gmail.com>
2021-03-08 12:24:24 -0800

    reset game & set initialize

--- script.js
+++ script.js
@@ -11,15 +11,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing =  true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function() {
+  playing = true; 
+  scores = [0, 0]; 
+  currentScore = 0;
+  activePlayer = 0;
+  diceEl.classList.remove('hidden');
+  document.getElementById('current--0').textContent = 0;
+  document.getElementById('current--1').textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  document.querySelector('.player--0').classList.remove('player--winner');
+  document.querySelector('.player--1').classList.remove('player--winner'); 
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+init();
 
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -67,3 +77,6 @@ btnHold.addEventListener('click', function() {
     }
   }
 });
+
+// Reset game
+btnNew.addEventListener('click', init);
